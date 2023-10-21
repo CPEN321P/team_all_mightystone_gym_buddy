c7618530a421677f834ef4c35f18e10563d24e7c
Savitoj Sachar <root@Savitoj-VivoBook>
2023-10-21 16:37:52 -0700

    server.js cleanup

--- backend/server.js
+++ backend/server.js
@@ -13,18 +13,6 @@ var server = app.listen(8081, (req,res)=>{
 })
 
 
-app.get("/name",(req,res) =>{
-    res.send("Savitoj Sachar")
-})
-
-app.get("/ip",(req,res) =>{
-    res.send("20.232.96.123")
-})
-
-app.get("/time", (req,res) =>{
-    const date = new Date()
-    res.send(date.getHours + ":" + date.getMinutes + ":" + date.getSeconds)
-})
 
 async function run(){
     try{
