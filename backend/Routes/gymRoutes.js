1a84f0ec3bf72922bfffff79a170982b947a0b3d
Tyson Brown <90238566+tysonpbr@users.noreply.github.com>
2023-11-03 14:46:16 -0700

    added try catch to gym routes

--- backend/Routes/gymRoutes.js
+++ backend/Routes/gymRoutes.js
@@ -13,100 +13,120 @@ const router = express.Router();
 
 // Create a new gym
 router.post('/', async (req, res) => {
-  const db = getDB();
+  try {
+    const db = getDB();
 
-  const newGym = {
-    name: req.body.name || "",
-    description: req.body.description || "",
-    location: req.body.location || "",
-    phone: req.body.phone || "",
-    email: req.body.email || "",
-    images: req.body.images || []
-    // hours
-  }
+    const newGym = {
+      name: req.body.name || "",
+      description: req.body.description || "",
+      location: req.body.location || "",
+      phone: req.body.phone || "",
+      email: req.body.email || "",
+      images: req.body.images || []
+      // hours
+    }
 
-  const result = await db.collection('gyms').insertOne(newGym);
+    const result = await db.collection('gyms').insertOne(newGym);
 
-  if (result && result.insertedId) {
-    res.status(200).json(result.insertedId.toString());
-  }
-  else {
+    if (result && result.insertedId) {
+      res.status(200).json(result.insertedId.toString());
+    }
+    else {
+      res.status(500).json("Gym not added to the database");
+    }
+  } catch (error) {
     res.status(500).json("Gym not added to the database");
   }
 });
 
 // Get all gyms
 router.get('/', async (req, res) => {
-  const db = getDB();
+  try {
+    const db = getDB();
 
-  const gyms = await db.collection('gyms').find().toArray();
+    const gyms = await db.collection('gyms').find().toArray();
 
-  if (gyms) {
-    res.status(200).json(gyms);
-  } else {
-    res.status(500).json("No gyms");
+    if (gyms) {
+      res.status(200).json(gyms);
+    } else {
+      res.status(404).json("No gyms found");
+    }
+  } catch (error) {
+    res.status(404).json("No gyms found");
   }
 });
 
 // Get a gym by id
 router.get('/gymId/:gymId', async (req, res) => {
-  const db = getDB();
-  const id = new ObjectId(req.params.gymId);
+  try {
+    const db = getDB();
+    const id = new ObjectId(req.params.gymId);
 
-  const gym = await db.collection('gyms').findOne({ _id: id });
+    const gym = await db.collection('gyms').findOne({ _id: id });
 
-  if (gym) {
-    res.status(200).json(gym);
-  } else {
+    if (gym) {
+      res.status(200).json(gym);
+    } else {
+      res.status(404).send('Gym not found');
+    }
+  } catch (error) {
     res.status(404).send('Gym not found');
   }
 });
 
 // Edit a gym by ID
 router.put('/gymId/:gymId', async (req, res) => {
-  const db = getDB();
-  const id = new ObjectId(req.params.gymId);
+  try {
+    const db = getDB();
+    const id = new ObjectId(req.params.gymId);
 
-  const gym = await db.collection('gyms').findOne({ _id: id });
+    const gym = await db.collection('gyms').findOne({ _id: id });
 
-  if (!gym) {
-    res.status(404).send('Gym not found');
-    return;
-  }
+    if (!gym) {
+      res.status(404).send('Gym not found');
+      return;
+    }
 
-  const updatedGym = {
-    name: req.body.name || gym.name,
-    description: req.body.description || gym.description,
-    location: req.body.location || gym.location,
-    phone: req.body.phone || gym.phone,
-    email: req.body.email || gym.email,
-    images: req.body.images || gym.images,
-    // hours
-  }
+    const updatedGym = {
+      name: req.body.name || gym.name,
+      description: req.body.description || gym.description,
+      location: req.body.location || gym.location,
+      phone: req.body.phone || gym.phone,
+      email: req.body.email || gym.email,
+      images: req.body.images || gym.images,
+      // hours
+    }
 
-  const result = await db.collection('gyms').updateOne(
-    { _id: id },
-    { $set: updatedGym }
-  );
-  
-  if (result.matchedCount == 0) {
-    res.status(404).send('Gym not found');
-  } else {
-    res.status(200).json(updatedGym);
+    const result = await db.collection('gyms').updateOne(
+      { _id: id },
+      { $set: updatedGym }
+    );
+    
+    if (result.matchedCount == 0) {
+      res.status(404).send('Gym not found');
+    } else {
+      res.status(200).json(updatedGym);
+    }
+  } catch (error) {
+    res.status(500).send('Gym not updated');
   }
 });
 
 // Delete a user by ID
 router.delete('/gymId/:gymId', async (req, res) => {
-  const db = getDB();
-  const id = new ObjectId(req.params.gymId);
-  
-  const result = await db.collection('gyms').deleteOne({ _id: id });
+  try {
+    const db = getDB();
+    const id = new ObjectId(req.params.gymId);
+    
+    const result = await db.collection('gyms').deleteOne({ _id: id });
 
-  if (result.deletedCount === 0) {
-    res.status(404).send('Gym not found');
-  } else {
-    res.status(200).send('Gym deleted successfully');
+    if (result.deletedCount === 0) {
+      res.status(404).send('Gym not found');
+    } else {
+      res.status(200).send('Gym deleted successfully');
+    }
+  } catch (error) {
+    res.status(500).send('Gym not deleted');
   }
 });
 
