2d7a0ab1c91c1509bd8301f7de61cf66bc4beb54
tysonpbr <thetysonbrown@gmail.com>
2023-11-28 13:55:44 -0800

    updated client JS

--- ClientJS/client.js
+++ ClientJS/client.js
@@ -1,5 +1,5 @@
 const io = require("socket.io-client");
-const socket = io("https://20.172.9.70:443");
+const socket = io("https://20.172.9.70:443", { rejectUnauthorized: false });
 
 socket.on('connect', async () => {
 	console.log('Client has connected to the server!');
